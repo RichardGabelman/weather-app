f3e1a2c5e88963dc63f8977d971dd53ddce99584
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-08-08 17:24:35 -0700

    Add getWeatherData async function

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,17 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 
+;// CONCATENATED MODULE: ./src/modules/weatherAPI.js
+async function getWeatherData(location) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=L9CTUTHMAGAWR5CNWQGG93NPC`;
+  let response = await fetch(url, {mode: "cors"});
+  return await response.json();
+}
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+getWeatherData("Oceanside").then(console.log);
 
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./style.css";
+import { getWeatherData } from "./modules/weatherAPI";
+
+getWeatherData("Oceanside").then(console.log);
