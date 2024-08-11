45a576807fc8b4bce1b2726c2319513fe8b4a04f
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-08-10 17:23:45 -0700

    Add forecast + emoji representation

--- src/modules/htmlGenerator.js
+++ src/modules/htmlGenerator.js
@@ -5,6 +5,7 @@ export class HTMLGenerator {
     if (!DataManager.currentWeatherObject) {
       return;
     }
+    const emoji = DataManager.currentWeatherObject.emoji;
     const address = DataManager.currentWeatherObject.location;
     const tempFahrenheit = DataManager.currentWeatherObject.temp;
     const desc = DataManager.currentWeatherObject.description;
@@ -18,7 +19,7 @@ export class HTMLGenerator {
     const unitToggle = document.querySelector(".unitToggle");
     const tempUnit = unitToggle.textContent;
     if (tempUnit == "C") {
-      const tempCelsius = (tempFahrenheit - 32)/1.8;
+      const tempCelsius = (tempFahrenheit - 32) / 1.8;
       tempDisplay.textContent = tempCelsius.toFixed(1) + "C";
     } else {
       tempDisplay.textContent = tempFahrenheit + "°F";
@@ -26,12 +27,40 @@ export class HTMLGenerator {
     tempDisplay.classList.add("temp");
 
     const descriptionDisplay = document.createElement("p");
-    descriptionDisplay.textContent = desc;
+    descriptionDisplay.textContent = emoji + " " + desc + " " + emoji;
 
     card.textContent = "";
     card.appendChild(unitToggle);
     card.appendChild(addressDisplay);
     card.appendChild(tempDisplay);
     card.appendChild(descriptionDisplay);
+
+    const smalls = document.querySelectorAll(".small");
+    for (let i = 0; i < smalls.length; i++) {
+      const currDay = DataManager.currentWeatherObject.forecast[i];
+      const temp = currDay.temp;
+      const desc = currDay.desc;
+      const date = currDay.date;
+      const emoji = currDay.emoji;
+
+      const tempDisplay = document.createElement("h3");
+      if (tempUnit == "C") {
+        const tempC = (temp - 32) / 1.8;
+        tempDisplay.textContent = tempC.toFixed(1) + "C";
+      } else {
+        tempDisplay.textContent = temp + "°F";
+      }
+
+      const descDisplay = document.createElement("p");
+      descDisplay.textContent = desc + " " + emoji;
+
+      const dateDisplay = document.createElement("p");
+      dateDisplay.textContent = date;
+
+      smalls[i].textContent = "";
+      smalls[i].append(tempDisplay);
+      smalls[i].append(descDisplay);
+      smalls[i].append(dateDisplay);
+    }
   }
 }
